5324f538e9cf5afddfec40c39aa3167f4048b281
dasdrolpi <74879793+dasdrolpi@users.noreply.github.com>
2022-03-16 18:37:35 +0100

    Change forEach of stream library to for loop in CleanUpTask

--- core/src/main/java/de/notion/pipeline/automatic/cleanup/CleanUpTask.java
+++ core/src/main/java/de/notion/pipeline/automatic/cleanup/CleanUpTask.java
@@ -3,7 +3,10 @@ package de.notion.pipeline.automatic.cleanup;
 import de.notion.pipeline.PipelineImpl;
 import de.notion.pipeline.annotation.resolver.AnnotationResolver;
 import de.notion.pipeline.config.PipelineRegistry;
+import de.notion.pipeline.datatype.PipelineData;
 import de.notion.pipeline.part.local.LocalCache;
+
+import java.util.UUID;
 
 public final class CleanUpTask implements Runnable {
 
@@ -19,26 +22,20 @@ public final class CleanUpTask implements Runnable {
 
     @Override
     public void run() {
-        registry.dataClasses()
-                .stream()
-                .forEach(dataClass -> {
-                    var optional = AnnotationResolver.cleanUp(dataClass);
+        for (var dataClass : registry.dataClasses()) {
+            AnnotationResolver.cleanUp(dataClass).ifPresent(cleanUp -> {
+                for (UUID uuid : localCache.savedUUIDs(dataClass)) {
+                    var data = localCache.data(dataClass, uuid);
+                    if (data == null)
+                        return;
+                    if ((System.currentTimeMillis() - data.lastUse()) < cleanUp.timeUnit().toMillis(cleanUp.time()))
+                        return;
 
-                    optional.ifPresent(cleanUp -> {
-                        var cachedUUIDs = localCache.savedUUIDs(dataClass);
-
-                        cachedUUIDs.forEach(uuid -> {
-                            var data = localCache.data(dataClass, uuid);
-                            if (data == null)
-                                return;
-                            if ((System.currentTimeMillis() - data.lastUse()) < cleanUp.timeUnit().toMillis(cleanUp.time()))
-                                return;
-
-                            System.out.println("Cleaning up " + dataClass.getSimpleName() + " with uuid " + uuid.toString());
-                            data.onCleanUp();
-                            pipelineImpl.cleanUpData(dataClass, data.objectUUID(), null);
-                        });
-                    });
-                });
+                    System.out.println("Cleaning up " + dataClass.getSimpleName() + " with uuid " + uuid.toString());
+                    data.onCleanUp();
+                    pipelineImpl.cleanUpData(dataClass, data.objectUUID(), null);
+                }
+            });
+        }
     }
 }
