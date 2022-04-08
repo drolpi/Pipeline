d2a21e8aacaa169d648b8df286e899a0731eea08
dasdrolpi <74879793+dasdrolpi@users.noreply.github.com>
2022-04-09 01:43:10 +0200

    Remove slf4j-nop dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,7 +29,6 @@ subprojects {
         compileOnly("com.google.guava:guava:23.0")
         compileOnly("org.reflections:reflections:0.10.2")
         compileOnly("org.jodd:jodd-core:5.3.0")
-        compileOnly("org.slf4j:slf4j-nop:1.7.36")
     }
 
     tasks.withType<JavaCompile> {
--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -5,7 +5,6 @@ dependencies {
     implementation("com.google.guava:guava:23.0")
     implementation("org.reflections:reflections:0.10.2")
     implementation("org.jodd:jodd-core:5.3.0")
-    implementation("org.slf4j:slf4j-nop:1.7.36")
 
     testImplementation(project(":mongodb"))
     testImplementation(project(":redis"))
