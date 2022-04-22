1904bfceb741ecf4cdfcb3cd1a2ff7e3bfc16be6
dasdrolpi <74879793+dasdrolpi@users.noreply.github.com>
2022-04-22 18:15:25 +0200

    Remove deleted modules from settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -17,13 +17,5 @@
 rootProject.name = "Pipeline"
 
 include(
-    ":core",
-    ":jsonfile",
-    ":mongodb",
-    ":redis",
-    ":sql",
-    ":mysql",
-    ":h2",
-    ":sqlite",
-    ":gson"
+    ":core"
 )
