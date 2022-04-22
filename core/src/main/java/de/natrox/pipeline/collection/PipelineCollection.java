cc2defbc667af046da87d707422220e5f4746b9b
dasdrolpi <74879793+dasdrolpi@users.noreply.github.com>
2022-04-22 22:56:39 +0200

    Add methods in PipelineCollection

--- core/src/main/java/de/natrox/pipeline/collection/PipelineCollection.java
+++ core/src/main/java/de/natrox/pipeline/collection/PipelineCollection.java
@@ -17,4 +17,25 @@
 package de.natrox.pipeline.collection;
 
 public interface PipelineCollection<T> {
+
+    void insert(T element);
+
+    void update(T element, boolean insertIfAbsent);
+
+    default void update(T element) {
+        this.update(element, false);
+    }
+
+    void remove(T element);
+
+    void drop();
+
+    boolean isDropped();
+
+    boolean isOpen();
+
+    long size();
+
+    void close();
+
 }
