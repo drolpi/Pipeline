e44d15cd1501ac4769732555a6cf8bee209e627e
dasdrolpi <74879793+dasdrolpi@users.noreply.github.com>
2022-04-23 06:41:18 +0200

    Move PartBundle implementations into PartBundle as sub classes

--- core/src/main/java/de/natrox/pipeline/PartBundle.java
+++ core/src/main/java/de/natrox/pipeline/PartBundle.java
@@ -18,15 +18,20 @@ package de.natrox.pipeline;
 
 import de.natrox.common.validate.Check;
 import de.natrox.pipeline.part.StoreManager;
+import de.natrox.pipeline.part.cache.DataUpdater;
+import de.natrox.pipeline.part.cache.GlobalCache;
+import de.natrox.pipeline.part.cache.LocalCache;
 import de.natrox.pipeline.part.cache.provider.DataUpdaterProvider;
 import de.natrox.pipeline.part.cache.provider.GlobalCacheProvider;
 import de.natrox.pipeline.part.cache.provider.LocalCacheProvider;
+import de.natrox.pipeline.part.storage.GlobalStorage;
+import de.natrox.pipeline.part.storage.LocalStorage;
 import de.natrox.pipeline.part.storage.provider.GlobalStorageProvider;
 import de.natrox.pipeline.part.storage.provider.LocalStorageProvider;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-public sealed interface PartBundle permits LocalBundle, GlobalBundle {
+public sealed interface PartBundle permits PartBundle.Local, PartBundle.Global {
 
     static @NotNull PartBundle local(
         @NotNull LocalStorageProvider localStorageProvider,
@@ -34,12 +39,12 @@ public sealed interface PartBundle permits LocalBundle, GlobalBundle {
     ) {
         Check.notNull(localStorageProvider, "localStorageProvider");
         Check.notNull(localCacheProvider, "localCacheProvider");
-        return new LocalBundle(localStorageProvider, localCacheProvider);
+        return new Local(localStorageProvider, localCacheProvider);
     }
 
     static @NotNull PartBundle local(@NotNull LocalStorageProvider localStorageProvider) {
         Check.notNull(localStorageProvider, "localStorageProvider");
-        return new LocalBundle(localStorageProvider);
+        return new Local(localStorageProvider);
     }
 
     static @NotNull PartBundle global(
@@ -48,7 +53,7 @@ public sealed interface PartBundle permits LocalBundle, GlobalBundle {
         @NotNull DataUpdaterProvider dataUpdaterProvider,
         @NotNull LocalCacheProvider localCacheProvider
     ) {
-        return new GlobalBundle(globalStorageProvider, globalCacheProvider, dataUpdaterProvider, localCacheProvider);
+        return new Global(globalStorageProvider, globalCacheProvider, dataUpdaterProvider, localCacheProvider);
     }
 
     static @NotNull PartBundle global(
@@ -56,20 +61,108 @@ public sealed interface PartBundle permits LocalBundle, GlobalBundle {
         @NotNull DataUpdaterProvider dataUpdaterProvider,
         @NotNull LocalCacheProvider localCacheProvider
     ) {
-        return new GlobalBundle(globalStorageProvider, dataUpdaterProvider, localCacheProvider);
+        return new Global(globalStorageProvider, dataUpdaterProvider, localCacheProvider);
     }
 
     static @NotNull PartBundle global(
         @NotNull GlobalStorageProvider globalStorageProvider,
         @NotNull GlobalCacheProvider globalCacheProvider
     ) {
-        return new GlobalBundle(globalStorageProvider, globalCacheProvider);
+        return new Global(globalStorageProvider, globalCacheProvider);
     }
 
     static @NotNull PartBundle global(@NotNull GlobalStorageProvider globalStorageProvider) {
-        return new GlobalBundle(globalStorageProvider);
+        return new Global(globalStorageProvider);
     }
 
     @NotNull StoreManager createStoreManager();
+
+    final class Local implements PartBundle {
+
+        private final LocalStorageProvider localStorageProvider;
+        private final LocalCacheProvider localCacheProvider;
+
+        protected Local(
+            @NotNull LocalStorageProvider localStorageProvider,
+            @Nullable LocalCacheProvider localCacheProvider
+        ) {
+            this.localStorageProvider = localStorageProvider;
+            this.localCacheProvider = localCacheProvider;
+        }
+
+        Local(LocalStorageProvider localStorageProvider) {
+            this(localStorageProvider, null);
+        }
+
+        @Override
+        public @NotNull StoreManager createStoreManager() {
+            LocalStorage storage = this.localStorageProvider.constructLocalStorage();
+
+            LocalCache localCache = null;
+            if (this.localCacheProvider != null)
+                localCache = this.localCacheProvider.constructLocalCache();
+
+            return new StoreManager(storage, null, null, localCache);
+        }
+
+    }
+
+    final class Global implements PartBundle {
+
+        private final GlobalStorageProvider globalStorageProvider;
+        private final GlobalCacheProvider globalCacheProvider;
+        private final DataUpdaterProvider dataUpdaterProvider;
+        private final LocalCacheProvider localCacheProvider;
+
+        Global(
+            @NotNull GlobalStorageProvider globalStorageProvider,
+            @Nullable GlobalCacheProvider globalCacheProvider,
+            @Nullable DataUpdaterProvider dataUpdaterProvider,
+            @Nullable LocalCacheProvider localCacheProvider
+        ) {
+            this.globalStorageProvider = globalStorageProvider;
+            this.globalCacheProvider = globalCacheProvider;
+            this.dataUpdaterProvider = dataUpdaterProvider;
+            this.localCacheProvider = localCacheProvider;
+        }
+
+        public Global(
+            @NotNull GlobalStorageProvider globalStorageProvider,
+            @Nullable DataUpdaterProvider dataUpdaterProvider,
+            @Nullable LocalCacheProvider localCacheProvider
+        ) {
+            this(globalStorageProvider, null, dataUpdaterProvider, localCacheProvider);
+        }
+
+        public Global(
+            @NotNull GlobalStorageProvider globalStorageProvider,
+            @Nullable GlobalCacheProvider globalCacheProvider
+        ) {
+            this(globalStorageProvider, globalCacheProvider, null, null);
+        }
+
+        public Global(@NotNull GlobalStorageProvider globalStorageProvider) {
+            this(globalStorageProvider, null);
+        }
+
+        @Override
+        public @NotNull StoreManager createStoreManager() {
+            GlobalStorage storage = globalStorageProvider.constructGlobalStorage();
+
+            GlobalCache globalCache = null;
+            if (globalCacheProvider != null)
+                globalCache = globalCacheProvider.constructGlobalCache();
+
+            DataUpdater dataUpdater = null;
+            LocalCache localCache = null;
+            if (dataUpdaterProvider != null && localCacheProvider != null) {
+                dataUpdater = dataUpdaterProvider.constructDataUpdater();
+                localCache = localCacheProvider.constructLocalCache();
+            }
+
+            return new StoreManager(storage, globalCache, dataUpdater, localCache);
+        }
+
+    }
 
 }
