0d71f1b1036be09af2d27685bbd1e431742d704c
dasdrolpi <74879793+dasdrolpi@users.noreply.github.com>
2022-04-24 01:48:49 +0200

    Add config module

--- config/build.gradle.kts
+++ config/build.gradle.kts
@@ -13,10 +13,10 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+dependencies {
 
-rootProject.name = "Pipeline"
+}
 
-include(
-    ":core",
-    ":config"
-)
+tasks.withType<Jar> {
+    archiveFileName.set("pipeline-config.jar")
+}
