42a05b4a6c26aa07094901c156e86185ad4bfacf
dasdrolpi <drolpiofficial@gmail.com>
2022-05-13 23:07:26 +0200

    Make Properties annotation accessible

--- core/src/main/java/de/natrox/pipeline/object/annotation/Properties.java
+++ core/src/main/java/de/natrox/pipeline/object/annotation/Properties.java
@@ -16,6 +16,13 @@
 
 package de.natrox.pipeline.object.annotation;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
 public @interface Properties {
 
     String identifier();
