0d127720fd3e902e07da79ef4b746ad4dc5c216a
dasdrolpi <drolpiofficial@gmail.com>
2022-05-15 23:19:40 +0200

    Change gradle description again

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,7 +25,7 @@ defaultTasks("build", "shadowJar")
 allprojects {
     group = "de.natrox"
     version = "2.0.0-SNAPSHOT"
-    description = "A fast, lightweight store written in Java that synchronizes records between databases and caches"
+    description = "A fast and lightweight store written in Java that synchronizes records between databases and caches"
 
     repositories {
         mavenCentral()
