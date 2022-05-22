82b7f73a915d58ac20aaa21760f76220e4982d89
dasdrolpi <drolpiofficial@gmail.com>
2022-05-22 21:43:12 +0200

    Clean up InstanceCreator

--- core/src/main/java/de/natrox/pipeline/object/InstanceCreator.java
+++ core/src/main/java/de/natrox/pipeline/object/InstanceCreator.java
@@ -22,6 +22,6 @@ import org.jetbrains.annotations.NotNull;
 @FunctionalInterface
 public interface InstanceCreator<T extends ObjectData> {
 
-    @NotNull T get(Class<? extends T> dataClass, Pipeline pipeline);
+    @NotNull T get(Class<? extends T> dataClass, Pipeline pipe);
 
 }
